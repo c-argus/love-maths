503b3a6cc769f69e5d4adecf880e404dd89775ef
c-argus <camila.argus88@gmail.com>
2022-10-27 13:32:10 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function chackAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubstractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
